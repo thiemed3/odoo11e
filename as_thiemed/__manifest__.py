d968d1b1369dc9767ab6e3cdf4c952dc9924e07d
phoenixsampras <phoenixsampras@gmail.com>
2020-08-15 17:46:07 -0400

    CRM 2.0

--- as_thiemed/__manifest__.py
+++ as_thiemed/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': "Ahorasoft Thiemed Project",
     'category': 'Thiemed',
-    'version': '1.1.0',
+    'version': '1.1.1',
     'author': "Ahorasoft",
     'website': 'http://www.ahorasoft.com',
     "support": "soporte@ahorasoft.com",
--- as_thiemed/models/as_pricelist_item.py
+++ as_thiemed/models/as_pricelist_item.py
@@ -3,4 +3,12 @@ from odoo import api, fields, models
 class product_pricelist_item(models.Model):
     _inherit = "product.pricelist.item"
     
-    default_code = fields.Char('Referencia Interna', related='product_tmpl_id.default_code')
+    default_code_item= fields.Char('Referencia Interna',compute='_onchange_product_tmpl')
+
+    @api.onchange('product_id','product_tmpl_id')
+    def _onchange_product_tmpl(self):
+        for rec in self:
+            if rec.product_tmpl_id:
+                rec.default_code_item=rec.product_tmpl_id.default_code
+            if rec.product_id:
+                rec.default_code_item=rec.product_id.default_code
